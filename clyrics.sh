8108e1faed8fd2429228e4e35dddfd5fce56cc8f
RebekkaRR <w3rbung@posteo.de>
2016-10-25 23:03:44 +0200

    Update clyrics.sh

--- clyrics.sh
+++ clyrics.sh
@@ -3,4 +3,4 @@ mkdir  /home/$user/.config/clyrics
 git clone https://github.com/trizen/clyrics.git /opt/
 apt install cpanminus
 cpanm WWW::Mechanize
-cp /opt/clyrics/plugin/*.pl /home/$user/.config/clyrics
+cp /opt/clyrics/plugins/*.pl /home/$user/.config/clyrics
