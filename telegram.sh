d991a35f295626ee0e2fc058ec44da0ac210af2c
RebekkaRR <w3rbung@posteo.de>
2016-10-26 22:26:13 +0200

    Add files via upload

---
+++ telegram.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo wget https://tdesktop.com/linux -O /opt/telegram-desktop.tar.gz
+sudo tar -Jxf /opt/telegram-desktop.tar.gz -C /opt/
+sudo ln -s /opt/Telegram/Telegram /usr/local/bin/telegram
+
+#this also works with arch linux
+#credit goes to linuxundich.de and telegram.org
