b29645b097f3302ec32a9e7dab4f18582f48a0e6
Rebekka <w3rbung@posteo.de>
2018-10-06 10:26:03 +0200

    Update the script
    Remove the tar.gz after installing

--- scripts/telegram.sh
+++ scripts/telegram.sh
@@ -2,6 +2,7 @@
 sudo wget https://tdesktop.com/linux -O /opt/telegram-desktop.tar.gz
 sudo tar -Jxf /opt/telegram-desktop.tar.gz -C /opt/
 sudo ln -s /opt/Telegram/Telegram /usr/local/bin/telegram
+sudo rm /opt/telegram-desktop.tar.gz
 
 #this also works with arch linux
 #credit goes to linuxundich.de and telegram.org
