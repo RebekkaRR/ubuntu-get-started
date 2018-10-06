fcab507c6d5652a61274d3b3d9ee68fe9ad94d84
Rebekka <w3rbung@posteo.de>
2018-10-06 10:35:16 +0200

    Require sudo for running

--- scripts/clyrics.sh
+++ scripts/clyrics.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 mkdir /home/$USER/.config/clyrics
-echo "Enter your password to clone clyrics to /opt directory and install&configure cpanminus"
-sleep 3
-sudo git clone https://github.com/trizen/clyrics.git /opt/clyrics
-sudo apt install cpanminus
-sudo cpanm WWW::Mechanize
-sudo cp /opt/clyrics/plugins/*.pl /home/$USER/.config/clyrics
-sudo ln -s /opt/clyrics/clyrics /usr/local/bin/clyrics
+if [ "$EUID" -ne 0 ]
+  then echo "Cannot detect root! Re-run as superuser"
+  exit
+fi
+git clone https://github.com/trizen/clyrics.git /opt/clyrics
+apt install cpanminus
+cpanm WWW::Mechanize
+cp /opt/clyrics/plugins/*.pl /home/$USER/.config/clyrics
+ln -s /opt/clyrics/clyrics /usr/local/bin/clyrics
